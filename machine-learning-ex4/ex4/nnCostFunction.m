409f4fd950bbdd71bd528eea074759330931241d
Preethi Kasireddy <iam.preethi.k@gmail.com>
2017-05-24 11:22:55 -0400

    Complete part 1 of assignment for calculating cost function

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,11 +62,33 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Part 1
+a1 = [ones(m, 1), X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2, 1), 1), a2];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
 
+hThetaX = a3;
 
+yVec = zeros(m, num_labels);
 
+for i = 1:m
+  yVec(i, y(i)) = 1;
+end; 
 
+% using for-loop
+for i = 1:m
+    term1 = -yVec(i,:) .* log(hThetaX(i,:));
+    term2 = (ones(1,num_labels) - yVec(i,:)) .* log(ones(1,num_labels) - hThetaX(i,:));
+    J = J + sum(term1 - term2);
+end
 
+J_2 = J / m;
+
+% using vectorized
+J = (1/m) * sum(sum(-1 * yVec .* log(hThetaX) - (1 - yVec) .* log(1 - hThetaX)));
 
 
 
