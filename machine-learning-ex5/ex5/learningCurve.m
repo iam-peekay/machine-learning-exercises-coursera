55047436bb0d83db2c917ea43f06a37cf1b3639c
Preethi Kasireddy <iam.preethi.k@gmail.com>
2017-05-24 17:56:39 -0400

    calculate learning curves

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -50,17 +50,15 @@ error_val   = zeros(m, 1);
 %           
 %       end
 %
+  for i = 1:m
+    X_sub = X(1:i, :);
+    y_sub = y(1:i);
 
-% ---------------------- Sample Solution ----------------------
+    % evaluate theta on training set
+    theta = trainLinearReg(X_sub, y_sub, lambda);
 
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
-
+    % calculate training error
+    error_train(i) = linearRegCostFunction(X_sub, y_sub, theta, 0);
+    % calculate cross-reference error (on the entire set) using theta calculated on the training set
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
 end
