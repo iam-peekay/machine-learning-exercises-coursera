123869684780858b6081bf0833e9584bb0a1068f
Preethi Kasireddy <iam.preethi.k@gmail.com>
2017-05-25 16:42:26 -0400

    Add code to init k-means centroids

--- machine-learning-ex7/ex7/kMeansInitCentroids.m
+++ machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -13,11 +13,12 @@ centroids = zeros(K, size(X, 2));
 %               the dataset X
 %
 
+% Randomly reorder the indices of examples
+randidx = randperm(size(X, 1));
 
 
-
-
-
+% Take the first K examples as centroids
+centroids = X(randidx(1:K), :);
 
 
 % =============================================================
